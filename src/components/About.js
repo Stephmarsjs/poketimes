eb0189150ef71f8915842a03c55bef529f25453a
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-29 00:41:37 -0400

    added about file

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+          <h4 className="center">About</h4>
+          <p>Lorem ipsum dolor sit amer consectetur, adipisicing elit.</p>
+        </div>
+    )
+}
+
+export default About
