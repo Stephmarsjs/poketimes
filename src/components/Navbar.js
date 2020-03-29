8aaab31c9cb86d3b90e2a84c28d6b2ddc404822b
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-29 02:02:32 -0400

    fixed spelling error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
          <div className="container">
            <a className="brand-logo">Poke'Times</a>
            <ul className="right">
-               <li><link to="/home">Home</link></li>
-               <li><link to="/about">About</link></li>
-               <li><link to="/contact">Contact</link></li>
+               <li><Link to="/home">Home</Link></li>
+               <li><Link to="/about">About</Link></li>
+               <li><Link to="/contact">Contact</Link></li>
             </ul> 
         </div>
      </nav>
