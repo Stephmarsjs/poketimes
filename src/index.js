1b2f792f3b803878a5678aaf221c572f88599d51
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-04-02 12:45:56 -0400

    added provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { creatStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const store = createStore();
 
