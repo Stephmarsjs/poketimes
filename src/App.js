86fc6a7b049fc9129fa6ea8656c60b205649b01b
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-04-02 11:48:13 -0400

    added img and switch tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
@@ -8,14 +8,16 @@ import Post from './components/Post'
 
 class App extends Component {
   render() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-     <Navbar />
-     <Route path='/home' component={Home} />
-     <Route path='/about' component={About} />
-     <Route path='/contact' component={Contact} />
-     <Route path="/:post_id" component={Post} />
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path="/:post_id" component={Post} />
+        </Switch>
     </div>
   </BrowserRouter>
   );
