dec316c4e21b71d2010b6df5ae12084c1dec9c88
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-04-06 12:35:49 -0400

    fixed length of undefined error

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-    posts: []
+    posts: [
+      {id: '1', title:'Squirtle Laid an Egg', body: 'Lorem ipsum, dolor sit amet'},
+      {id: '2', title:'Charmander Laid an Egg', body:'Lorem ipsum, dolor sit amet'},
+      {id: '3', title:'a Helix Fossil was Found', body: 'Lorem ipsum, dolor sit amet'}  
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
